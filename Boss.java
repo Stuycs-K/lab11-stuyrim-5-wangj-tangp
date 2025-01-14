356d1991121066da2325bdbc22606773337b4ee6
Patrick Tang <patrickt74@nycstudents.net>
2025-01-14 11:33:45 -0500

    Fixed some methods

--- Boss.java
+++ Boss.java
@@ -6,7 +6,7 @@ public class Boss extends Adventurer{
     this.setName("Mewtwo");
     this.HP=1000;
     this.maxHP=1000;
-    this.energy=200;
+    this.energy=50;
     this.maxEnergy=200;
     this.dmgBoost=1;
     }
@@ -37,17 +37,14 @@ public class Boss extends Adventurer{
     int damage= (int)(Math.random() * 1.15 * 140);
     other.applyDamage(damage*dmgBoost);
     restoreSpecial(60);
-    int currentHP = getHP();
-    setHP(getHP() + damage);
-    int finalHP = getHP();
     return "Mewtwo used Aura Sphere, dealing " + damage + "damage.";
   }
 
   public String specialAttack(Adventurer other){
     int damage = (int)(Math.random() * 1.15 * 280);
-    if(getSpecial()>=60){
+    if(getSpecial()>=200){
       other.applyDamage(damage);
-      setSpecial(getSpecial()-60);
+      setSpecial(getSpecial()-200);
     }else{
       return "Mewtwo tried to use Psystrike but didnt have enough energy. Instead, " + attack(other);
     }
